3685fb7dfb9302a0abc0ce38a4934c133dd8b4b0
Mr Sheerluck <mrsheerluck003@gmail.com>
2026-06-29 19:23:39 +0530

    add client struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,1 @@
-pub fn hello() -> &'static str {
-    "Hello from eik"
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(hello(), "Hello from eik");
-    }
-}
+pub mod clients;
